92b081c98ca12b2220c2e53153e954b7368d2e21
John Adkerson <61293447+AdkersonJohn@users.noreply.github.com>
2026-08-08 12:38:59 -0400

    Updates

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-// Expose protected methods that allow the renderer process to use
-// the ipcRenderer without exposing the entire object
-contextBridge.exposeInMainWorld("electronAPI", {
-  newSearch: () => ipcRenderer.send("new-search"),
-  onNewSearch: (callback) => ipcRenderer.on("new-search", callback),
-});
